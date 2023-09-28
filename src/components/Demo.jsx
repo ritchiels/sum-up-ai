f0a11c4412eb16a30addfe34cda66eb381539f67
Ritchie Simmons <ritchiels.alt@gmail.com>
2023-09-27 23:32:55 -0400

    api setup next

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { copy, linkIcon, loader, tick } from '../assets';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
+    const [article, setArticle] = useState({
+        url: '',
+        summary: '',
+    });
+
+    const handleSubmit = async (e) => {
+        
+    }
+
+    return (
+        <section className="mt-16 w-full max-w-xl">
+            {/* Search */}
+            <div className="flex flex-col w-full gap-2">
+                <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
+                    <img
+                        src={linkIcon}
+                        alt="link_icon"
+                        className="absolute left-0 my-2 ml-3 w-5"
+                    />
+                    <input
+                        type="url"
+                        placeholder="Enter a URL"
+                        value={article.url}
+                        onChange={(e) => setArticle({ ...article, url: e.target.value })}
+                        required
+                        className="url_input peer"
+                    />
+                    <button
+                        type="submit"
+                        className="submit_btn"
+                    >
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
+                </form>
+                {/* Browse URL History */}
+
+            </div>
+
+            {/* Display Results */}
+        </section>
+    )
 }
 
 export default Demo
+
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,14 +15,15 @@ const Hero = () => {
         </nav>
 
         <h1 className="head_text">
-            Summarize articles with <br />
-            <span className="orange_gradient">OpenAI GPT-4</span>
+            Summarize articles with <br className="max-md:hidden" />
+            <span className="blue_gradient">OpenAI GPT-4</span>
         </h1>
+        <h2 className="desc">
+            Simplify your reading with Sum Up, an open-source article summarizer that transforms lengthy articles into clear and concise summaries.
+        </h2>
     </header>
   )
 }
 
 export default Hero
 
-//22:32
-//streak
